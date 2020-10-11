e41d6fef959da1da20710ce3d749e104cdeeff4e
Devin Don <I.INF@Outlook.com>
2020-10-11 18:56:37 +0800

    feat(@util): export function & load config

---
+++ src/main/@util/index.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+
+export interface Config {
+  IMEI: string;
+  UIN: string;
+  KEY: string;
+  DATABASE: string;
+}
+
+export function loadConfig(): Config {
+  return JSON.parse(readFileSync('key.json').toString());
+}
+
+export function getDate() {
+  const date = new Date();
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+}
