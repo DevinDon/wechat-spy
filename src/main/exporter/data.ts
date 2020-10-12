183b3e2ac691e097985bf9fb6585d173241e0dd5
Devin Don <I.INF@Outlook.com>
2020-10-12 19:30:43 +0800

    feat(exporter): clean data

--- src/main/exporter/data.ts
+++ src/main/exporter/data.ts
@@ -1,10 +1,22 @@
-import { MessageEntity } from '../message/message.entity';
 import { createQueryBuilder } from 'typeorm';
+import { MessageEntity } from '../message/message.entity';
+import { MessageType } from '../message/message.type';
+
+const REG_AT_NAME = /(@助教.*?)\s/;
 
 export async function exportData() {
-  const messages = (await createQueryBuilder(MessageEntity)
-    .select(['content'])
-    .andWhere('content LIKE :keyword', { keyword: '%助教%' })
-    .getMany()).map(message => message.content);
-  return messages;
+  const messages = await createQueryBuilder(MessageEntity)
+    .select()
+    .andWhere('"type" = :type', { type: MessageType.Normal })
+    .andWhere('"content" LIKE :keyword', { keyword: '%助教%' })
+    .getMany();
+  const array = messages
+    .map(message => {
+      const text = message.content;
+      return REG_AT_NAME.test(text) ? text.match(REG_AT_NAME)! : '';
+    })
+    .flat()
+    .filter(name => name.length < 10 && name.length > 0)
+    .map(name => name.trim());
+  return [...new Set(array)];
 }
