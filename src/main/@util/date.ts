b8d1a8d925030d1513c9b72dcfeb2cf8ebd706a5
Devin Don <I.INF@Outlook.com>
2020-10-13 19:20:13 +0800

    perf(@util): remove useless code

--- src/main/@util/date.ts
+++ src/main/@util/date.ts
@@ -1,17 +1,3 @@
-import { readFileSync } from 'fs';
-
-export interface Config {
-  IMEI: string;
-  UIN: string;
-  KEY: string;
-  DATABASE: string;
-  ID: string;
-}
-
-export function loadConfig(): Config {
-  return JSON.parse(readFileSync('key.json').toString());
-}
-
 export function getDate() {
   const date = new Date();
   const year = date.getFullYear().toString().padStart(4, '0');
