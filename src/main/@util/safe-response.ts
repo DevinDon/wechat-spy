90593aab54ae2de205c59e9dfb0b95bf9da5651d
Devin Don <I.INF@Outlook.com>
2020-10-19 22:56:28 +0800

    feat(@util): add safe response utils

--- src/main/@util/safe-response.ts
+++ src/main/@util/safe-response.ts
@@ -2,3 +2,14 @@ export function safeResponse(value: any) {
   delete value._id;
   return value;
 }
+
+export function safeResponseFields(value: any, keys: string[] = ['_id']) {
+  for (const key of keys) {
+    delete value[key];
+  }
+  return value;
+}
+
+export function safeResponseArray(array: any[]) {
+  return array.map(safeResponse);
+}
